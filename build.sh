785d11d76244b621e5dce0dab3805b5466f5fd64
sidneyoliveira <sidneyoliveira2015@gmail.com>
2025-09-25 16:16:33 -0300

    create superuser

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Comandos para instalar dependências e preparar o banco de dados
 pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
 
-python manage.py collectstatic --noinput
-
-python manage.py migrate --noinput
-
-
-if [[$CREATE_SUPERUSER]]; 
-then
-    python manage.py createsuperuser --no-input  --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+# --- SCRIPT DE CRIAÇÃO DO SUPERUSUÁRIO (CORRIGIDO) ---
+if [[ "$CREATE_SUPERUSER" == "true" ]]; then
+  echo "Creating superuser..."
+  # O comando vai ler as variáveis de ambiente DJANGO_SUPERUSER_* automaticamente
+  python manage.py createsuperuser --no-input
+  echo "Superuser created."
 fi
