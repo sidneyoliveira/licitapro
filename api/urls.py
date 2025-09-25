196dd4861223f89b406f180b91fa7546c0a4893c
sidneyoliveira <sidneyoliveira2015@gmail.com>
2025-09-25 17:08:58 -0300

    fix axios

--- api/urls.py
+++ api/urls.py
@@ -1,30 +1,26 @@
-# api/urls.py
+# backend/api/urls.py
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 from .views import (
-    ProcessoViewSet, 
-    OrgaoViewSet, 
-    FornecedorViewSet, 
-    MunicipioViewSet,  
-    CreateUserView,
-    ManageUserView,
-    DashboardStatsView
+    ProcessoViewSet, OrgaoViewSet, FornecedorViewSet, MunicipioViewSet,
+    CreateUserView, ManageUserView, DashboardStatsView
 )
 
 router = DefaultRouter()
-router.register(r'processos', ProcessoViewSet)
-router.register(r'orgaos', OrgaoViewSet)
-router.register(r'fornecedores', FornecedorViewSet)
+router.register(r'processos', ProcessoViewSet, basename='processo')
+router.register(r'fornecedores', FornecedorViewSet, basename='fornecedor')
+router.register(r'orgaos', OrgaoViewSet, basename='orgao')
 router.register(r'municipios', MunicipioViewSet, basename='municipio')
 
 urlpatterns = [
     path('', include(router.urls)),
-    # Rotas de Autenticação JWT
+    path('dashboard-stats/', DashboardStatsView.as_view(), name='dashboard_stats'),
+    path('register/', CreateUserView.as_view(), name='register'),
+    path('me/', ManageUserView.as_view(), name='me'),
+    
+    # Esta é a rota que a função de login procura
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', CreateUserView.as_view(), name='register'),
-    path('dashboard-stats/', DashboardStatsView.as_view(), name='dashboard_stats'),
-    path('me/', ManageUserView.as_view(), name='me'),
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
+# backend/backend/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
 ]
