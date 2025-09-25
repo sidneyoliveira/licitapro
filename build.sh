9e2e27f275c7c4d62315b80efa067651a3ad6e60
sidneyoliveira <sidneyoliveira2015@gmail.com>
2025-09-25 15:47:00 -0300

    create superuser

--- build.sh
+++ build.sh
@@ -5,3 +5,9 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate --noinput
+
+
+if [[$CREATE_SUPERUSER == "true"]]; 
+then
+    python manage.py createsuperuser --noinput
+fi
