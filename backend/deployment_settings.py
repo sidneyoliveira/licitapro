18d6ecd52eb52e97b31a0733e9d1d90db285a370
sidneyoliveira <sidneyoliveira2015@gmail.com>
2025-09-25 22:09:18 -0300

    fix axios

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -22,10 +22,10 @@ MIDDLEWARE = [
 
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
 
 STORAGES = {
 
