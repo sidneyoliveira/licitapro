fc564c0e8ad9ef58d18a7a0405af70e3c9a710ac
sidneyoliveira <sidneyoliveira2015@gmail.com>
2025-10-07 12:53:37 -0300

    alteracoes no views

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 # backend/api/admin.py
 
 from django.contrib import admin
-from .models import CustomUser, Entidade, Orgao, ProcessoLicitatorio, ItemProcesso, ItemCatalogo
+from .models import CustomUser, Entidade, Orgao, ProcessoLicitatorio, ItemProcesso
 
 # Registos básicos que já tínhamos
 admin.site.register(CustomUser)
