65468124ac6579dc12ee9867b1eae40f60acdd55
sidneyoliveira <sidneyoliveira2015@gmail.com>
2025-10-08 08:48:06 -0300

    alteracoes no backend

--- api/admin.py
+++ api/admin.py
@@ -12,9 +12,27 @@ from .models import (
 
 @admin.register(ItemProcesso)
 class ItemProcessoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'descricao', 'unidade', 'quantidade', 'processo', 'ordem')
+    list_display = ('id', 'processo', 'item_catalogo', 'quantidade', 'ordem')
     list_filter = ('processo',)
-    search_fields = ('descricao', 'especificacao')
+    search_fields = ('item_catalogo__descricao',)
+
+    def save_model(self, request, obj, form, change):
+        """
+        Sobrescrevemos o método de salvar do admin.
+        Esta função é chamada sempre que um ItemProcesso é salvo através da interface de admin.
+        """
+        # Se for um objeto novo (não uma edição)
+        if not obj.pk and obj.processo:
+            # Encontra a ordem mais alta para os itens deste processo e adiciona 1.
+            try:
+                ordem_max = ItemProcesso.objects.filter(processo=obj.processo).latest('ordem').ordem
+                obj.ordem = ordem_max + 1
+            except ItemProcesso.DoesNotExist:
+                # Se for o primeiro item do processo, a ordem é 1.
+                obj.ordem = 1
+        
+        # Continua com o processo normal de salvar o objeto.
+        super().save_model(request, obj, form, change)
 
 @admin.register(Fornecedor)
 class FornecedorAdmin(admin.ModelAdmin):
