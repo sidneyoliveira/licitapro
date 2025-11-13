52bd75fbb1e967d4408453f015a2c9458a67873c
sidneyoliveira <sidneyoliveira2015@gmail.com>
2025-11-13 17:12:19 -0300

    add orgao

--- api/models.py
+++ api/models.py
@@ -75,12 +75,12 @@ class ProcessoLicitatorio(models.Model):
     modalidade = models.CharField(
         max_length=50,
         choices=[
-            (1, 'Pregão Eletrônico'),
-            (2, 'Concorrência Eletrônica'),
-            (3, 'Dispensa Eletrônica'),
-            (4, 'Inexigibilidade Eletrônica'),
-            (5, 'Adesão a Registro de Preços'),
-            (6, 'Credenciamento'),
+            ('1', 'Pregão Eletrônico'),
+            ('2', 'Concorrência Eletrônica'),
+            ('3', 'Dispensa Eletrônica'),
+            ('4', 'Inexigibilidade Eletrônica'),
+            ('5', 'Adesão a Registro de Preços'),
+            ('6', 'Credenciamento'),
         ],
     )
     classificacao = models.CharField(
