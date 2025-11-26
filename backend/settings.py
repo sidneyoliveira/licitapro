2068e686054ea70b4d80c359cc981cad7e22b000
sidneyoliveira <sidneyoliveira2015@gmail.com>
2025-11-26 10:16:05 -0300

    add setting

--- backend/settings.py
+++ backend/settings.py
@@ -119,6 +119,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
